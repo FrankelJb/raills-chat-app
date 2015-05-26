b4579843b585e4199d0a1c2558a2bd4199c9afa9
Jonathan Frankel <jonathan.frankel@multichoice.co.za>
2015-05-26 08:48:06 +0200

    Heroku debugging

--- lib/chat_backend.rb
+++ lib/chat_backend.rb
@@ -11,7 +11,7 @@ class ChatBackend
   def initialize app
     @app = app
     @clients = []
-    uri = URI.parse(ENV['REDISTOGO_URL'])
+    uri = URI.parse('redis://localhost:6379')
     @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
     Thread.new do
       redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
